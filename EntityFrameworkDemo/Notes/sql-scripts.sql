2d6c7491d6e96deb7b223f1e0c214953145f5d69
sarath baiju <sarath.bu@frontiersin.net>
2022-01-05 15:47:05 +0530

    Notes added

---
+++ EntityFrameworkDemo/Notes/sql-scripts.sql
@@ -0,0 +1,8 @@
+﻿create database Learning_Db;
+--------------------------------------
+
+create table Students (ID int primary key identity(1,1),
+FirstName varchar(30),
+LastName varchar(30),
+EnrollmentDate DateTime
+);
