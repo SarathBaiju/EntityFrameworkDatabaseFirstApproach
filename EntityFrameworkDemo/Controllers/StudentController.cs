6e9dbe5d76a81148b65b26ab7b737570b343994e
sarath baiju <sarath.bu@frontiersin.net>
2022-01-05 15:43:17 +0530

    Add project files.

---
+++ EntityFrameworkDemo/Controllers/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using EntityFrameworkDemo.Database;
+using EntityFrameworkDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentDbContext _studentDbContext;
+
+        public StudentController(StudentDbContext studentDbContext)
+        {
+            this._studentDbContext = studentDbContext;
+        }
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_studentDbContext.Students.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddStudent([FromBody] Student student)
+        {
+            try
+            {
+                _studentDbContext.Students.Add(student);
+                _studentDbContext.SaveChanges();
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost("add-many")]
+        public IActionResult AddStudent([FromBody] IEnumerable<Student> students)
+        {
+            try
+            {
+                _studentDbContext.Students.AddRange(students);
+                _studentDbContext.SaveChanges();
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpPut("update")]
+        public IActionResult UpdateStudent([FromBody] Student student)
+        {
+            try
+            {
+                _studentDbContext.Students.Update(student);
+                _studentDbContext.SaveChanges();
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            try
+            {
+                var student = _studentDbContext.Students.Find(id);
+                _studentDbContext.Students.Remove(student);
+                _studentDbContext.SaveChanges();
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ EntityFrameworkDemo/Database/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EntityFrameworkDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.Database
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> contextOptions):base(contextOptions)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
